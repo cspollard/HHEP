8925c1b63defe519f9ae9a1d2a51e1e137e8ca5e
Chris Pollard <cspollard@gmail.com>
2016-03-28 00:25:45 +0100

    getting something sensible out of plotting!

--- app/drawPoints.hs
+++ app/drawPoints.hs
@@ -2,7 +2,8 @@ module Main where
 
 
 import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
+-- import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Backend.PGF.CmdLine
 import Graphics.Histo
 
 import Data.Histogram
@@ -10,6 +11,16 @@ import Data.Histogram
 hTest :: Histo1D
 hTest = histogram (constBin1D 10 (0, 10)) mempty `fill` (1.0, Z :. 1.5) `fill` (2.5, Z :. 2.5) `fill` (0.5, Z :. 2.5)
 
+hTest' :: Histo1D
+hTest' = histogram (constBin1D 10 (0, 10)) mempty `fill` (1.0, Z :. 1.5) `fill` (0.5, Z :. 3.5) `fill` (0.5, Z :. 0.5)
+
 main :: IO ()
--- main = mainWith (point ((1.0, (0.25, 0.5)), (0.0, (0.5, 0.25))) # lw 10 # showOrigin:: Diagram B)
-main = mainWith (drawHist hTest # lw 10 # showOrigin :: Diagram B)
+main = (mainWith :: Diagram B -> IO ()) $
+        let h  = drawGraph $ histToGraph hTest
+            h' = drawGraph $ histToGraph hTest'
+        in mconcat [ h  # lc blue # lw 05 # dashing [10, 10] 0
+                   , h' # lc red  # lw 05
+                   , yAxis h
+                   , xAxis h
+                   , text "$e = mc^2$" # fontSizeG 1 # fc green # translateX 5 # translateY 2
+                   ] # centerXY # pad 1.1
--- src/Graphics/Histo.hs
+++ src/Graphics/Histo.hs
@@ -5,23 +5,41 @@ module Graphics.Histo where
 import Data.TypeList
 import Diagrams.Prelude
 import Data.Histogram
-import Control.Arrow ((&&&))
 
+
+type PtErr2D = ((Double, (Double, Double)), (Double, (Double, Double)))
+
+type Graph2D = [PtErr2D]
 
 dup :: a -> (a, a)
 dup x = (x, x)
 
-drawHist :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) =>
-            Histo1D -> Diagram b
-drawHist h = mconcat . map (point . toPoint) $ toTuples h
+histToGraph :: Histo1D -> Graph2D
+histToGraph = map toPoint . toTuples
     where
         toPoint ((Z :. xlo, Z :. xhi), d :. _) = let x0 = (xlo+xhi)/2.0 in
                                     ((x0, (xhi-x0, x0-xlo)), let xw = (xhi-xlo) in (sumw d / xw, dup (uncert d / xw)))
         uncert d = sqrt(sumw2 d)
 
 
-point :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) =>
-         ((Double, (Double, Double)), (Double, (Double, Double))) -> Diagram b
-point ((x0, (xlo, xhi)), (y0, (ylo, yhi))) = hrule (xlo+xhi) # translate (r2 (x0 + (xhi-xlo)/2.0, y0))
+
+drawGraph :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) =>
+             Graph2D -> Diagram b
+drawGraph = mconcat . map drawPoint
+
+
+drawPoint :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => PtErr2D -> Diagram b
+drawPoint ((x0, (xlo, xhi)), (y0, (ylo, yhi))) = hrule (xlo+xhi) # translate (r2 (x0 + (xhi-xlo)/2.0, y0))
                                              `atop`
                                              vrule (ylo+yhi) # translate (r2 (x0, y0 + (yhi-ylo)/2.0))
+
+
+xAxis :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => Diagram b -> Diagram b
+xAxis d = case extentX d of
+               Nothing       -> hrule 1
+               Just (lo, hi) -> hrule (hi-lo) # translateX ((hi+lo)/2)
+
+yAxis :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => Diagram b -> Diagram b
+yAxis d = case extentY d of
+               Nothing       -> vrule 1
+               Just (lo, hi) -> vrule (hi-lo) # translateY ((hi+lo)/2)
