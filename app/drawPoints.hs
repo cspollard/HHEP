da1abbf3d25db769dd1d2dfc27e080d9890ea1c4
Chris Pollard <cspollard@gmail.com>
2016-03-28 18:18:22 +0100

    need to work out font scaling.

--- app/drawPoints.hs
+++ app/drawPoints.hs
@@ -4,23 +4,24 @@ module Main where
 import Diagrams.Prelude
 -- import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Backend.PGF.CmdLine
-import Graphics.Histo
+import Data.Colour.Palette.ColorSet
 
+import Graphics.Histo
 import Data.Histogram
 
+import Debug.Trace
+
 hTest :: Histo1D
-hTest = histogram (constBin1D 10 (0, 10)) mempty `fill` (1.0, Z :. 1.5) `fill` (2.5, Z :. 2.5) `fill` (0.5, Z :. 2.5)
+hTest = histogram (constBin1D 10 (0, 10)) mempty `fill` (-1.0, Z :. 1.5) `fill` (2.5, Z :. 2.5) `fill` (0.5, Z :. 2.5)
 
 hTest' :: Histo1D
-hTest' = histogram (constBin1D 10 (0, 10)) mempty `fill` (1.0, Z :. 1.5) `fill` (0.5, Z :. 3.5) `fill` (0.5, Z :. 0.5)
+hTest' = histogram (constBin1D 10 (0, 5)) mempty `fill` (1.0, Z :. 1.5) `fill` (0.5, Z :. 3.5) `fill` (0.5, Z :. 0.5)
 
 main :: IO ()
 main = (mainWith :: Diagram B -> IO ()) $
         let h  = drawGraph $ histToGraph hTest
             h' = drawGraph $ histToGraph hTest'
-        in mconcat [ h  # lc blue # lw 05 # dashing [10, 10] 0
-                   , h' # lc red  # lw 05
-                   , yAxis h
-                   , xAxis h
-                   , text "$e = mc^2$" # fontSizeG 1 # fc green # translateX 5 # translateY 2
-                   ] # centerXY # pad 1.1
+        in mconcat [ h  # lc (d3Colors1 0)
+                   , h' # lc (d3Colors1 1)
+                   -- , text "$e = mc^2$" # fontSizeG 1 # fc (d3Colors1 2) # translateX 5 # translateY 2
+                   ] # scaleToX 1 # scaleToY 5 # addAxes # centerXY # pad 1.1 # lwN 0.005
--- src/Graphics/Histo.hs
+++ src/Graphics/Histo.hs
@@ -2,10 +2,16 @@
 
 module Graphics.Histo where
 
+import Diagrams.Prelude hiding (diff)
+import Diagrams.TwoD.Text
+import Numeric (showGFloat)
+
+import Data.Maybe (fromMaybe)
+
 import Data.TypeList
-import Diagrams.Prelude
 import Data.Histogram
 
+import Debug.Trace
 
 type PtErr2D = ((Double, (Double, Double)), (Double, (Double, Double)))
 
@@ -22,24 +28,81 @@ histToGraph = map toPoint . toTuples
         uncert d = sqrt(sumw2 d)
 
 
+-- TODO
+-- there must be a better way to do this...
+line :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b))
+     => P2 Double -> P2 Double -> Diagram b
+line xyi xyf = let (xi, yi) = unp2 xyi
+                   (xf, yf) = unp2 xyf
+                in trailLike $ trailFromSegments [straight $ r2 (xf-xi, yf-yi)] `at` xyi
+
+hline :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b))
+      => Double -> Double -> Diagram b
+hline xi xf = line (mkP2 xi 0) (mkP2 xf 0)
+
+vline :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b))
+      => Double -> Double -> Diagram b
+vline yi yf = line (mkP2 0 yi) (mkP2 0 yf)
+
 
 drawGraph :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) =>
              Graph2D -> Diagram b
-drawGraph = mconcat . map drawPoint
+drawGraph = mconcat . map drawPoint . traceShowId
 
 
 drawPoint :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => PtErr2D -> Diagram b
-drawPoint ((x0, (xlo, xhi)), (y0, (ylo, yhi))) = hrule (xlo+xhi) # translate (r2 (x0 + (xhi-xlo)/2.0, y0))
-                                             `atop`
-                                             vrule (ylo+yhi) # translate (r2 (x0, y0 + (yhi-ylo)/2.0))
+drawPoint ((x0, (xlo, xhi)), (y0, (ylo, yhi))) = hline (x0-xlo) (x0+xhi) # translateY y0
+                                                 `atop`
+                                                 vline (y0-ylo) (y0+yhi) # translateX x0
 
 
-xAxis :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => Diagram b -> Diagram b
-xAxis d = case extentX d of
-               Nothing       -> hrule 1
-               Just (lo, hi) -> hrule (hi-lo) # translateX ((hi+lo)/2)
+addAxes :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b), Renderable (Text Double) b)
+      => Diagram b -> Diagram b
+addAxes d = let (xlo, xhi) = fromMaybe (0, 1) $ extentX d
+                (ylo, yhi) = fromMaybe (0, 1) $ extentY d
+                xscale = xhi - xlo
+                yscale = yhi - ylo
+            in mconcat [ d
+                       , translateY ylo $ hline xlo xhi `atop` xIterEvery xlo xhi (tickSpacing xscale) (labTickX $ 0.03*yscale)
+                       , translateX xlo $ vline ylo yhi `atop` yIterEvery ylo yhi (tickSpacing yscale) (labTickY $ 0.03*xscale)
+                       ]
 
-yAxis :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b)) => Diagram b -> Diagram b
-yAxis d = case extentY d of
-               Nothing       -> vrule 1
-               Just (lo, hi) -> vrule (hi-lo) # translateY ((hi+lo)/2)
+
+-- determine the approrpriate tick spacing for a given scale
+tickSpacing :: Double -> Double
+tickSpacing s = traceShowId $ 10.0 ^^ ((floor :: Double -> Int) . logBase 10 . (/4.0) $ s)
+
+
+-- TODO
+-- it's possible for these to return one item too few if strt and stp
+-- are separated by an integer multiple of dx/dy.
+xIterEvery :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b))
+           => Double -> Double -> Double -> (Double -> Diagram b) -> Diagram b
+xIterEvery strt stp dx d = mconcat . fmap (\x -> d x # translateX x) . takeWhile (<= stp) $ iterate (+dx) strt
+
+yIterEvery :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b))
+           => Double -> Double -> Double -> (Double -> Diagram b) -> Diagram b
+yIterEvery strt stp dy d = mconcat . fmap (\y -> d y # translateY y) . takeWhile (<= stp) $ iterate (+dy) strt
+
+
+-- TODO
+-- don't like normalized font size here.
+labTickX, minTickX, labTickY, minTickY
+    :: (InSpace V2 Double (Diagram b), TrailLike (Diagram b), Renderable (Text Double) b)
+    => Double -> Double -> Diagram b
+labTickX s x = vrule 1.0 # scale s
+              `atop` text (showGFloat (Just 1) x "") # translateY (-1*s) # fontSizeN 0.03
+minTickX s = const $ vrule 0.5 # scale s
+
+labTickY s y = text (showGFloat (Just 1) y "") # translateX (-1.5*s) # fontSizeN 0.03
+               `atop` hrule 1.0 # scale s
+minTickY s = const $ hrule 0.5 # scale s
+
+
+-- TODO
+-- some utility that certainly exists elsewhere...
+diff :: Num a => a -> a -> a
+diff = (-)
+
+avg :: Fractional a => a -> a -> a
+avg x y = (x+y) / 2
