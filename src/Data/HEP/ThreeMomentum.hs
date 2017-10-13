152de14a9170d1df88f5f94f1a3e057004c87f3e
Chris Pollard <cspollard@gmail.com>
2017-10-13 15:52:14 +0200

    forgot file

---
+++ src/Data/HEP/ThreeMomentum.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DeriveFunctor   #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.HEP.ThreeMomentum where
+
+import           Control.Lens
+import           Data.Serialize
+import           GHC.Generics   (Generic)
+
+data XYZ a =
+  XYZ
+  { _vx :: {-# UNPACK #-} !a
+  , _vy :: {-# UNPACK #-} !a
+  , _vz :: {-# UNPACK #-} !a
+  } deriving (Show, Generic, Functor)
+
+makeLenses ''XYZ
+instance Serialize a => Serialize (XYZ a) where
+
+inner :: Num a => XYZ a -> XYZ a -> a
+inner (XYZ x y z) (XYZ x' y' z') = (x*x') + (y*y') + (z*z')
+
+cross :: Num a => XYZ a -> XYZ a -> XYZ a
+cross (XYZ x y z) (XYZ x' y' z') =
+  XYZ (y*z' - z*y') (z*x' - x*z') (x*y' - x'*y)
+
+modulus :: Num a => XYZ a -> a
+modulus xyz = inner xyz xyz
